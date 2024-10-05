3570097db673e79053313d6bb4d201bcda9bd300
ragib sina <ragib.sina@gmail.com>
2024-10-05 16:22:24 -0600

    add hero image

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   title: "MediCal",
   description:
     "SaaS solution for appointment booking, patient and doctor management.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
