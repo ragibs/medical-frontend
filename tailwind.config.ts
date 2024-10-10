0a9ff70bdf9cd5e39aac25089c468301ec0a96a2
ragib sina <ragib.sina@gmail.com>
2024-10-10 12:49:33 -0600

    updated tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,15 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   primary: "#3490dc", // Customize the primary color (e.g., blue)
+      //   "primary-foreground": "#ffffff", // Customize the text color for primary background if needed
+      //   background: "#e5e5e5", // This is the custom background color
+      //   foreground: "#3f3f46", // Custom text color for foreground
+      //   // You can also add other colors like primary, secondary, etc.
+      // },
+    },
   },
   plugins: [],
 };
